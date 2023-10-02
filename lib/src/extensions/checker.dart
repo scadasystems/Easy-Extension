338b4f78613e3a1b006341301610c3a44ee65001
LulzM <yunmo159789@gmail.com>
2023-10-02 16:27:35 +0900

    Change vpn active to return null

--- lib/src/extensions/checker.dart
+++ lib/src/extensions/checker.dart
@@ -1,21 +1,25 @@
 part of easy_extension;
 
 class Checker {
-  static Future<bool> isVpnActive() async {
-    bool isVpnActive;
+  static Future<bool?> isVpnActive() async {
+    try {
+      bool isVpnActive;
 
-    List<NetworkInterface> interfaces = await NetworkInterface.list(
-      includeLoopback: false,
-      type: InternetAddressType.any,
-    );
+      List<NetworkInterface> interfaces = await NetworkInterface.list(
+        includeLoopback: false,
+        type: InternetAddressType.any,
+      );
 
-    interfaces.isNotEmpty
-        ? isVpnActive = interfaces.any((interface) =>
-            interface.name.contains("tun") ||
-            interface.name.contains("ppp") ||
-            interface.name.contains("pptp"))
-        : isVpnActive = false;
+      interfaces.isNotEmpty
+          ? isVpnActive = interfaces.any((interface) =>
+              interface.name.contains("tun") ||
+              interface.name.contains("ppp") ||
+              interface.name.contains("pptp"))
+          : isVpnActive = false;
 
-    return isVpnActive;
+      return isVpnActive;
+    } catch (_) {
+      return null;
+    }
   }
 }
