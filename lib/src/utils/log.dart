0edffa3b1f2eff2816b9d21e88827b237a90d95c
LulzM <lulzm@dicamo.com>
2023-10-26 15:51:37 +0900

    Outputting logs only in debug mode

--- lib/src/utils/log.dart
+++ lib/src/utils/log.dart
@@ -12,29 +12,47 @@ import 'package:flutter/foundation.dart';
 /// - [Log.white] : White log
 class Log {
   /// Normal Log to console
-  static void d(dynamic text) => debugPrint('$text');
+  static void d(dynamic text) {
+    if (kDebugMode) debugPrint('$text');
+  }
 
   /// Black Log to console
-  static void black(dynamic text) => debugPrint('\x1B[30m$text\x1B[0m');
+  static void black(dynamic text) {
+    if (kDebugMode) debugPrint('\x1B[30m$text\x1B[0m');
+  }
 
   /// Red Log to console
-  static void red(dynamic text) => debugPrint('\x1B[31m$text\x1B[0m');
+  static void red(dynamic text) {
+    if (kDebugMode) debugPrint('\x1B[31m$text\x1B[0m');
+  }
 
   /// Green Log to console
-  static void green(dynamic text) => debugPrint('\x1B[32m$text\x1B[0m');
+  static void green(dynamic text) {
+    if (kDebugMode) debugPrint('\x1B[32m$text\x1B[0m');
+  }
 
   /// Yellow Log to console
-  static void yellow(dynamic text) => debugPrint('\x1B[33m$text\x1B[0m');
+  static void yellow(dynamic text) {
+    if (kDebugMode) debugPrint('\x1B[33m$text\x1B[0m');
+  }
 
   /// Blue Log to console
-  static void blue(dynamic text) => debugPrint('\x1B[34m$text\x1B[0m');
+  static void blue(dynamic text) {
+    if (kDebugMode) debugPrint('\x1B[34m$text\x1B[0m');
+  }
 
   /// Magenta Log to console
-  static void magenta(dynamic text) => debugPrint('\x1B[35m$text\x1B[0m');
+  static void magenta(dynamic text) {
+    if (kDebugMode) debugPrint('\x1B[35m$text\x1B[0m');
+  }
 
   /// Cyan Log to console
-  static void cyan(dynamic text) => debugPrint('\x1B[36m$text\x1B[0m');
+  static void cyan(dynamic text) {
+    if (kDebugMode) debugPrint('\x1B[36m$text\x1B[0m');
+  }
 
   /// White Log to console
-  static void white(dynamic text) => debugPrint('\x1B[37m$text\x1B[0m');
+  static void white(dynamic text) {
+    if (kDebugMode) debugPrint('\x1B[37m$text\x1B[0m');
+  }
 }
