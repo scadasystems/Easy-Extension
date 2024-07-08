be58ceeedafe7ebbc853470b648dc8bf64228bcf
LulzM <yunmo159789@gmail.com>
2024-07-08 13:45:30 +0900

    Add: num.sizedBox

--- lib/src/extensions/widget.dart
+++ lib/src/extensions/widget.dart
@@ -3,6 +3,8 @@ part of '../../easy_extension.dart';
 extension SizedboxExtension on num {
   Widget get widthBox => SizedBox(width: toDouble());
   Widget get heightBox => SizedBox(height: toDouble());
+
+  Widget get sizedBox => SizedBox(width: toDouble(), height: toDouble());
 }
 
 extension GapExtension on num {
