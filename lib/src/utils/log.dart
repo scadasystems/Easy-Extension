43a2e5954f0af3d5dbc7979dc1548ed37417a108
LulzM <yunmo159789@gmail.com>
2024-11-18 15:49:37 +0900

    Upgrade Plugins, Update Log

--- lib/src/utils/log.dart
+++ lib/src/utils/log.dart
@@ -11,48 +11,59 @@ import 'package:flutter/foundation.dart';
 /// - [Log.cyan] : Cyan log
 /// - [Log.white] : White log
 class Log {
-  /// Normal Log to console
-  static void d(dynamic text) {
-    if (kDebugMode) debugPrint('$text');
+  static bool _isDebugMode = kDebugMode;
+
+  static void setDebugMode(bool isDebugMode) {
+    _isDebugMode = isDebugMode;
   }
 
-  /// Black Log to console
-  static void black(dynamic text) {
-    if (kDebugMode) debugPrint('\x1B[30m$text\x1B[0m');
+  /// Normal Log to console
+  static void d(dynamic text) {
+    if (_isDebugMode) debugPrint('$text');
   }
 
   /// Red Log to console
   static void red(dynamic text) {
-    if (kDebugMode) debugPrint('\x1B[31m$text\x1B[0m');
+    if (_isDebugMode) debugPrint('\x1B[31m$text\x1B[0m');
+  }
+
+  /// Orange Log to console
+  static void orange(dynamic text) {
+    if (_isDebugMode) debugPrint('\x1B[38;5;208m$text\x1B[0m');
   }
 
   /// Green Log to console
   static void green(dynamic text) {
-    if (kDebugMode) debugPrint('\x1B[32m$text\x1B[0m');
-  }
-
-  /// Yellow Log to console
-  static void yellow(dynamic text) {
-    if (kDebugMode) debugPrint('\x1B[33m$text\x1B[0m');
+    if (_isDebugMode) debugPrint('\x1B[32m$text\x1B[0m');
   }
 
   /// Blue Log to console
   static void blue(dynamic text) {
-    if (kDebugMode) debugPrint('\x1B[34m$text\x1B[0m');
+    if (_isDebugMode) debugPrint('\x1B[34m$text\x1B[0m');
+  }
+
+  /// Yellow Log to console
+  static void yellow(dynamic text) {
+    if (_isDebugMode) debugPrint('\x1B[33m$text\x1B[0m');
   }
 
   /// Magenta Log to console
   static void magenta(dynamic text) {
-    if (kDebugMode) debugPrint('\x1B[35m$text\x1B[0m');
+    if (_isDebugMode) debugPrint('\x1B[35m$text\x1B[0m');
   }
 
   /// Cyan Log to console
   static void cyan(dynamic text) {
-    if (kDebugMode) debugPrint('\x1B[36m$text\x1B[0m');
+    if (_isDebugMode) debugPrint('\x1B[36m$text\x1B[0m');
   }
 
   /// White Log to console
   static void white(dynamic text) {
-    if (kDebugMode) debugPrint('\x1B[37m$text\x1B[0m');
+    if (_isDebugMode) debugPrint('\x1B[37m$text\x1B[0m');
+  }
+
+  /// Black Log to console
+  static void black(dynamic text) {
+    if (_isDebugMode) debugPrint('\x1B[30m$text\x1B[0m');
   }
 }
