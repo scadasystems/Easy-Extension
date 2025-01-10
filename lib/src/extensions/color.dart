1ab3a1110567fa00eded9a5ac2ad7613db7a6f04
LulzM <yunmo159789@gmail.com>
2025-01-10 13:54:25 +0900

    Color.toValue for SDK 3.27.0 ~

--- lib/src/extensions/color.dart
+++ lib/src/extensions/color.dart
@@ -1,6 +1,14 @@
 part of '../../easy_extension.dart';
 
 extension ColorExtension on Color {
+  int get toValue {
+    final a = (this.a * 255).round() & 0xff;
+    final r = (this.r * 255).round() & 0xff;
+    final g = (this.g * 255).round() & 0xff;
+    final b = (this.b * 255).round() & 0xff;
+    return (a << 24) | (r << 16) | (g << 8) | b;
+  }
+
   String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
       '${alpha.toRadixString(16).padLeft(2, '0')}'
       '${red.toRadixString(16).padLeft(2, '0')}'
